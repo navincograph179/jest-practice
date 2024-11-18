c2d044a2e80012a6e911a2f219293034cf7697a9
navincograph179 <navin_gurung@co-graph.com>
2024-11-18 15:09:30 +0900

    with axios

--- functions.js
+++ functions.js
@@ -1,6 +1,17 @@
+const axios = require('axios')
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull : () => null,
+  checkValue : a => a,
+
+  createUser: () => {
+    const user = { firstName: 'Navin' };
+    user['lastName'] = 'Gurung';
+    return user;
+  },
+  fetchUser : () => axios.get('https://jsonplaceholder.typicode.com/users/1').then(res => res.data)
+  .catch(err => 'error' )
 }
 
 //module.exports = sum, is sum function is exported for sum.test.js
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,4 @@
 const functions = require('./functions');
-const sum = require('./functions');
 
 test('adds 1 + 2 equal to 3', () => {
   expect(functions.add(1,2)).toBe(3);
@@ -20,4 +19,51 @@ test('adds 1 + 2 is not equal to ', () => {
 
 test("Should be null", () => {
   expect(functions.isNull()).toBeNull();
+})
+
+test("Should be Falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+})
+
+test("Should be Falsy", () => {
+  expect(functions.checkValue(undefined)).toBeFalsy();
+})
+
+// toBe is not work for objects
+// toEqual
+test("Should be get user fullName", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Navin',
+    lastName: 'Gurung'
+  })
+})
+
+// Less than and greater than
+
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+})
+
+// Regex(Regular expression)
+
+test('There is no I in Team', () => {
+  expect('team').not.toMatch(/I/);
+})
+
+// Arrays
+test('Admin should be in username', () => {
+  userames = ["John", "Michel", "Jackson"];
+  expect(userames).toContain('John');
+})
+
+
+// Working with async data
+
+test('User Fetch name should be Leanne Graham', () => {
+  expect.assertions(1);
+  return functions.fetchUser().then(data => {
+    expect(data.name).toEqual('Leanne Graham');
+  })
 })
