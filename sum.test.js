07cfda8f4810e8bf782393f786235a40927b6466
navincograph179 <navin_gurung@co-graph.com>
2024-11-18 14:25:46 +0900

    comments

--- sum.test.js
+++ sum.test.js
@@ -9,3 +9,9 @@ test('adds 1 + 2 equal to 3', () => {
   expect(sum(1,2)).not.toBe(5);
 })
 
+// CHECK FOR TRUTHY & FALSY VALUES
+
+// toBeNull matches only NULL
+// toBeUndefined matches only undefined
+// toBeTruthy matches anything that an if statement treates as true.
+// toBeFalsy matches anything that an if statement treats as false.
